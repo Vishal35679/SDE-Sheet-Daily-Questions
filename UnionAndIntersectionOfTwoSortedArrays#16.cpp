b7d85aeb01270f440b2defc22fbb8d592ffadce7
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-04-23 18:38:08 +0530

    DAY - 5 #16 Union and Intersection of two Sorted Arrays

---
+++ UnionAndIntersectionOfTwoSortedArrays#16.cpp
@@ -0,0 +1,77 @@
+Union and Intersection of two Sorted Arrays
+link: https://www.geeksforgeeks.org/union-and-intersection-of-two-sorted-arrays-2/
+
+Problem:
+Given two sorted arrays, find their union and intersection.
+  
+CODE:
+// C++ program to find union of two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+void printUnion(int arr1[], int arr2[], int m, int n)
+{
+	int i = 0, j = 0;
+	while (i < m && j < n) {
+		if (arr1[i] < arr2[j])
+			cout << arr1[i++] << " ";
+
+		else if (arr2[j] < arr1[i])
+			cout << arr2[j++] << " ";
+
+		else {
+			cout << arr2[j++] << " ";
+			i++;
+		}
+	}
+  
+	while (i < m)
+		cout << arr1[i++] << " ";
+
+	while (j < n)
+		cout << arr2[j++] << " ";
+}
+
+
+int main()
+{
+	int arr1[] = { 1, 2, 4, 5, 6 };
+	int arr2[] = { 2, 3, 5, 7 };
+	int m = sizeof(arr1) / sizeof(arr1[0]);
+	int n = sizeof(arr2) / sizeof(arr2[0]);
+	printUnion(arr1, arr2, m, n);
+	return 0;
+}
+
+
+
+// C++ program to find intersection of two sorted arrays
+#include <bits/stdc++.h>
+using namespace std;
+
+void printIntersection(int arr1[], int arr2[], int m, int n)
+{
+	int i = 0, j = 0;
+	while (i < m && j < n) {
+		if (arr1[i] < arr2[j])
+			i++;
+		else if (arr2[j] < arr1[i])
+			j++;
+		else /* if arr1[i] == arr2[j] */
+		{
+			cout << arr2[j] << " ";
+			i++;
+			j++;
+		}
+	}
+}
+
+int main()
+{
+	int arr1[] = { 1, 2, 4, 5, 6 };
+	int arr2[] = { 2, 3, 5, 7 };
+	int m = sizeof(arr1) / sizeof(arr1[0]);
+	int n = sizeof(arr2) / sizeof(arr2[0]);
+	printIntersection(arr1, arr2, m, n);
+	return 0;
+}
