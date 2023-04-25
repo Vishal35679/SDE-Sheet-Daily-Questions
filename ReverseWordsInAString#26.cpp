395cceb176a0fd7e42895869809769e2a0dd0dcd
Vishal35679 <104795331+Vishal35679@users.noreply.github.com>
2023-04-25 22:36:37 +0530

    DAY - 8 #26 Reverse words in a String

---
+++ ReverseWordsInAString#26.cpp
@@ -0,0 +1,30 @@
+Reverse Words in a String
+link: https://leetcode.com/problems/reverse-words-in-a-string/
+
+Problem:
+Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+  
+CODE:
+class Solution {
+public:
+    string reverseWords(string s) {
+        if(s.size() == 0) return s;
+        stack<string> stack;
+        string result;
+        for(int i=0; i<s.size(); i++) {
+            string word;
+            if(s[i]==' ') continue; //skip spaces
+            while(i<s.size() && s[i]!=' ' ) { //store continuous letters into word
+                word += s[i]; i++;
+            }
+            stack.push(word); //push word to the stack
+        }
+        while(!stack.empty()) {
+            result += stack.top(); stack.pop();
+            if(!stack.empty()) result += " ";
+        }
+        return result;
+    }
+};
